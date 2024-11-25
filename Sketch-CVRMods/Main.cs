1175f8cff3a8e48a9c5b7b62b2eeecf061cc3af5
SketchFoxsky <109103755+SketchFoxsky@users.noreply.github.com>
2024-11-24 22:41:48 -0500

    Initial Push

---
+++ Sketch-CVRMods/Main.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+using ABI_RC.Systems.Movement;
+using ABI_RC.Core.UI;
+using ABI.CCK.Components;
+
+namespace Sketch.SourceMovement
+{
+    public class SourceMovement : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
