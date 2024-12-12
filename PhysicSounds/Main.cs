e52e2f78f6fe31dd09ecb5701369ff73a47a13ac
SketchFoxsky <109103755+SketchFoxsky@users.noreply.github.com>
2024-12-12 00:43:23 -0500

    Physics Sounds Init

---
+++ PhysicSounds/Main.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+using ABI_RC.Core.Util.AssetFiltering;
+
+namespace Sketch.PhysicSounds
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            SharedFilter.SpawnableWhitelist.Add(typeof(PhysicSound));
+            SharedFilter.LocalComponentWhitelist.Add(typeof(PhysicSound));
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
--- SourceMovement/obj/Debug/netstandard2.1/SourceMovement.AssemblyInfo.cs
+++ SourceMovement/obj/Debug/netstandard2.1/SourceMovement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SourceMovement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1aad54f4d2ea5afcef6d404ffea292d2dfe44ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08bd629a945386f30a909c596da0698e05c0fb1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SourceMovement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SourceMovement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
