0de6db8621d1e716c8e663ab08005a70c12a5cd2
SketchFoxsky <109103755+SketchFoxsky@users.noreply.github.com>
2024-12-12 00:44:49 -0500

    PhysicsSounds Unity Folder

---
+++ PhysicSounds/Components/PhysicSound/Scripts/PhysicSound.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sketch.PhysicSounds
+{
+    [AddComponentMenu("SketchFoxsky/ModdedCCK/PhysicSounds")]
+
+    public class PhysicSound : MonoBehaviour
+    {
+        public AudioSource AudioSourceReference;
+        public AudioClip[] MinCollisionAudio;
+        public AudioClip[] MiddleCollisionAudio;
+        public AudioClip[] MaxCollisionAudio;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var ColMag = collision.relativeVelocity.magnitude;
+
+            if ((ColMag >= 0.1f) && (ColMag <= 4.9))
+            {
+                var clip = MinCollisionAudio[Random.Range(0, MinCollisionAudio.Length)];
+                Debug.Log("Small");
+                AudioSourceReference.clip = clip;
+                AudioSourceReference.Play();
+            }
+            if ((ColMag >= 5f) && (ColMag <= 10))
+            {
+                var clip = MiddleCollisionAudio[Random.Range(0, MinCollisionAudio.Length)];
+                Debug.Log("Mid");
+                AudioSourceReference.clip = clip;
+                AudioSourceReference.Play();
+            }
+            if (ColMag >= 10.01f)
+            {
+                var clip = MaxCollisionAudio[Random.Range(0, MinCollisionAudio.Length)];
+                Debug.Log("Large");
+                AudioSourceReference.clip = clip;
+                AudioSourceReference.Play();
+            }
+        }
+    }
+}
+
