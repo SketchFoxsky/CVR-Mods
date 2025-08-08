f8077ce54f1b3a3c05351cfdd043047abca0822e
SketchFoxsky <109103755+SketchFoxsky@users.noreply.github.com>
2025-08-08 01:39:05 -0400

    VRCLightVolume Mod
    Partial Code available Due to all the components and scripts being from REDSIM

---
+++ red.sim.LightVolumes/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using red.sim.LightVolumes.Properties;
+using MelonLoader;
+using System.Reflection;
+
+[assembly: MelonInfo(
+    typeof(red.sim.LightVolumes.Main),
+    nameof(red.sim.LightVolumes),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "N/A"
+)]
+
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(255, 3, 252, 78)]
+[assembly: MelonAuthorColor(255, 40, 144, 209)] 
+[assembly: HarmonyDontPatchAll]
+
+namespace red.sim.LightVolumes.Properties
+{
+    internal static class AssemblyInfoParams
+    {
+        public const string Version = "1.0.0";
+        public const string Author = "REDSIM , SketchFoxsky";
+    }
+}
---
+++ red.sim.LightVolumesUdon/Main.cs
@@ -0,0 +1,28 @@
+﻿using ABI_RC.API;
+using ABI_RC.Core.Util.AssetFiltering;
+using MelonLoader;
+using VRCLightVolumes;
+
+namespace red.sim.LightVolumesUdon
+
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            //World Whitelist
+            WorldFilter._Base.Add(typeof(LightVolume));
+            WorldFilter._Base.Add(typeof(LightVolumeData));
+            WorldFilter._Base.Add(typeof(LightVolumeDataSorter));
+            WorldFilter._Base.Add(typeof(LightVolumeInstance));
+            WorldFilter._Base.Add(typeof(LightVolumeManager));
+            WorldFilter._Base.Add(typeof(LightVolumeSetup));
+            WorldFilter._Base.Add(typeof(PointLightVolume));
+            WorldFilter._Base.Add(typeof(PointLightVolumeInstance));
+            //Prop Whitelist
+            SharedFilter.SpawnableWhitelist.Add(typeof(PointLightVolumeInstance));
+            SharedFilter.SpawnableWhitelist.Add(typeof(PointLightVolume));
+            MelonLogger.Msg("Initialized, now whitelisting modded components!");
+        }
+    }
+}
